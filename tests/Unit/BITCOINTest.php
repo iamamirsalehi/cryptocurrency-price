954e51d3cfd2be99ada489ba937add21612056c1
iamamirsalehi <iamamirsalehi@gmail.com>
2021-02-26 07:43:25 -0500

    Bitcoin test implemented

--- tests/Unit/BITCOINTest.php
+++ tests/Unit/BITCOINTest.php
@@ -12,4 +12,12 @@ class BITCOINTest extends TestCase
         $this->assertNotNull($bitcoin_long_price);
         $this->assertIsNumeric($bitcoin_long_price);
     }
+
+    public function test_if_crawler_can_get_bitcoin_short_price()
+    {
+        $bitcoin_short_price = CryptoCurrencies::bitcoin()->short();
+
+        $this->assertNotNull($bitcoin_short_price);
+        $this->assertIsNumeric($bitcoin_short_price);
+    }
 }
